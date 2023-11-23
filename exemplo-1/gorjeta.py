1612685c6c9cb4bc4d5fc11f2ff1fb8cca3a4173
gabriel <gabriel.exposto1357@gmail.com>
2023-11-22 22:07:53 -0300

    comentarios no programa em python

--- exemplo-1/gorjeta.py
+++ exemplo-1/gorjeta.py
@@ -1,3 +1,6 @@
+#O objeteivo do programa é gerar uma figura que analize a porcentagem de gorjeta que um garçom recebe
+#tendo como parametros de avaliação a nota da comida e a nota do atendimento
+
 # bibliotecas importadas
 import numpy as np
 import matplotlib.pyplot as plt 
@@ -8,11 +11,11 @@ serv,com = np.meshgrid(s, c) # junção dos dois vetores
 
 g1 = ((0.02/20) * (serv + com)) + 0.05 # função gorgeta
 
-fig = plt.figure() 
-ax = fig.add_subplot(111, projection='3d') 
-ax.plot_surface(serv, com, g1) 
-ax.set_title('gorgeta') 
-ax.set_xlabel('serv') 
-ax.set_ylabel('com')
+fig = plt.figure() # Criação da figura
+ax = fig.add_subplot(111, projection='3d') # subplot para gerar uma projeção 3d 
+ax.plot_surface(serv, com, g1)  # informações para os eixos x,y,z
+ax.set_title('gorgeta') # titulo da figura
+ax.set_xlabel('serv') # titulo do eixo x
+ax.set_ylabel('com') # titulo do eixo Y
 
-plt.show()
+plt.show() # mostra a figura
